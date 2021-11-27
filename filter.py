a20db93b4e7983f03a9efad7036e4d264833314a
Gladioluss <emurashin@gmail.com>
2021-11-27 12:21:39 +0500

    3 этап

--- filter.py
+++ filter.py
@@ -10,16 +10,13 @@ def convert_to_grayscale(pixels_array, size_mosaic, grayscale, row_length, colum
         while column_index < column_length - (size_mosaic - 1):
             color = 0
             for step_row in range(row_index, row_index + size_mosaic):
-                for step_column in range(column_index, column_index + size_mosaic):
-                    color += (int(pixels_array[step_row][step_column][0])
-                              + int(pixels_array[step_row][step_column][1])
-                              + int(pixels_array[step_row][step_column][2])) // 3
-            medium_brightness = color // size_mosaic ** 2
+                color += sum(map(lambda step_column:
+                                 sum(map(int, pixels_array[step_row][step_column])) // 3,
+                                 range(column_index, column_index + size_mosaic)))
+                medium_brightness = ((color // size_mosaic ** 2) // grayscale) * grayscale
             for step_row in range(row_index, row_index + size_mosaic):
                 for step_column in range(column_index, column_index + size_mosaic):
-                    pixels_array[step_row][step_column][0] = (medium_brightness // grayscale) * grayscale
-                    pixels_array[step_row][step_column][1] = (medium_brightness // grayscale) * grayscale
-                    pixels_array[step_row][step_column][2] = (medium_brightness // grayscale) * grayscale
+                    pixels_array[step_row][step_column] = np.full(3, medium_brightness)
             column_index += size_mosaic
         row_index += size_mosaic
     return pixels_array
@@ -28,6 +25,6 @@ def convert_to_grayscale(pixels_array, size_mosaic, grayscale, row_length, colum
 image_array = np.array(Image.open("img2.jpg"))
 Image.fromarray(convert_to_grayscale(image_array,
                                     int(input("Укажите размер мозаики:")),
-                                    int(input("укажите градации серого:")),
+                                    int(input("Укажите градации серого:")),
                                     len(image_array),
                                     len(image_array[1]))).save('res.jpg')
